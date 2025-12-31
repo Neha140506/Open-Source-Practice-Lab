d3b7e9c09c87bac24fd01b2a8d2698e0cac7717d
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2025-12-31 21:49:31 +0530

    added filter method

--- tasks/javascript/easy/filter-numbers.js
+++ tasks/javascript/easy/filter-numbers.js
@@ -11,6 +11,17 @@ Output:
 
 const numbers = [2, 5, 8, 10, 12, 15, 19, 20, 25];
 
-// TODO: Write your solution here
+//one line solution by using filter funtion !!! #easy #no complexity
 
-console.log(result);
+//const result = numbers.filter(num => num > 10 && num < 20 );
+
+//longer solution using for loop
+let arrays = [];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 10 && numbers[i] < 20) {
+    arrays.push(numbers[i]);
+  }
+}
+console.log(arrays);
+
+//depends on you wich one you want to go for!!!
