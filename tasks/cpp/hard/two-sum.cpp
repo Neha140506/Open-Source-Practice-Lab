aa74abccacfda20eac754873c10c146609a38639
Dineshkanna.R <dineshkanna8072@gmail.com>
2026-01-12 17:01:23 +0530

    Two Sum is Solved

--- tasks/cpp/hard/two-sum.cpp
+++ tasks/cpp/hard/two-sum.cpp
@@ -13,6 +13,21 @@ Indices: 0, 1
 using namespace std;
 
 int main() {
-    // TODO: Solve two sum using hashing
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int n = sizeof(nums) / sizeof(nums[0]);
+    int l = 0, r = n - 1;
+    while (l < r) {
+        int sum = nums[l] + nums[r];
+        if (sum == target) {
+            cout << "Indices: " << l << "," << r << endl;
+            return 0;
+        } else if (sum < target) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+    cout << "No pair found" << endl;
     return 0;
 }
